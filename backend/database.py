6d2f76eeeb3d228850831c0d3370df389909f89b
Hayrunnisa Yoltan <hayrunnisayoltan@gmail.com>
2026-05-23 01:15:40 +0300

    update

--- backend/database.py
+++ backend/database.py
@@ -3,7 +3,7 @@ from motor.motor_asyncio import AsyncIOMotorClient
 # MongoDB Atlas'tan kopyaladığınız adresi buraya yapıştırın.
 # <username> ve <password> alanlarını kendi belirlediğiniz bilgilerle doldurun.
 # Şifrenizde özel karakterler (@, /, : vb.) varsa sorun yaşamamak için şifrenizi sade seçebilir veya url-encode edebilirsiniz.
-MONGO_DETAILS = "mongodb+srv://hayrunnisayoltan_db_user:1377nisa.@cluster0.fbi5zb2.mongodb.net/?appName=Cluster0"
+MONGO_DETAILS = os.getenv("MONGO_URL", "mongodb://localhost:27017")
 
 client = AsyncIOMotorClient(MONGO_DETAILS)
 
